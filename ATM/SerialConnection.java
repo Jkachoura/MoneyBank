254e9542acbe2c9dd1b7f26a3d5936afc6f26afb
Sam Yost <90903248+samyost1@users.noreply.github.com>
2022-03-31 12:01:12 +0200

    Update and rename SerialInterface.java to SerialConnection.java
    Hernoemd en efficiëntie van het algoritme verbeterd

---
+++ ATM/SerialConnection.java
@@ -0,0 +1,55 @@
+import com.fazecast.jSerialComm.*;
+
+public class SerialConnection {
+
+    private SerialPort comPort;
+    private String input = null;
+
+    public SerialConnection() {
+        comPort = SerialPort.getCommPorts()[0];
+        comPort.setBaudRate(9600);
+        comPort.openPort();
+
+        comPort.addDataListener(new SerialPortDataListener() {
+            @Override
+            public int getListeningEvents() {
+                return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
+            }
+
+            @Override
+            public void serialEvent(SerialPortEvent event) {
+                if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE) return;
+
+                byte[] newData = new byte[comPort.bytesAvailable()]; //receive incoming bytes
+                comPort.readBytes(newData, newData.length); //read incoming bytes
+                input = new String(newData); //convert bytes to string
+
+//                System.out.println(input);
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+            }
+        });
+    }
+
+    public void clearInput() {
+        input = null;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void giveOutput(String data) {
+        System.out.println(data);
+        try {
+            byte[] b = data.getBytes();
+            comPort.writeBytes(b, b.length);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
