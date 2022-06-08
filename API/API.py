7af268a2508ff779ce02c63f0b137fcf65f5173c
Raekwon Killop <91854246+Raekwon1603@users.noreply.github.com>
2022-06-08 13:15:20 +0200

    API.py 4.0
    Onnodige endpoints verwijderd. Verdere optimalisatie van de code

--- API/API.py
+++ API/API.py
@@ -1,8 +1,12 @@
 import dbm
+import requests
+import json
+
 from flask import Flask, jsonify, request, make_response
 import mysql.connector
 
 app = Flask(__name__)
+
 
 moneybank = mysql.connector.connect(
     host="localhost",
@@ -10,6 +14,7 @@ moneybank = mysql.connector.connect(
     password="MNBK22",
     database="moneybank"
 )
+
 mycursor = moneybank.cursor()
 
 
@@ -21,11 +26,24 @@ def check_balance():
     try:
         data= request.json
         if not "IBAN" in data or not "Pincode" in data:
+            
+
             return(make_response(jsonify("Wrong body!"), 400))
 
         iban = data["IBAN"]
-        pincode = data["Pincode"]
+        
 
+        pincode = data["Pincode"]
+        # check blockstatus
+        mycursor.execute(f"SELECT BlockStatus FROM accounts WHERE IBAN = '{iban}'")
+        results = mycursor.fetchone()
+        
+        if (int(results[0]) == 1):
+            mycursor.execute(f"UPDATE accounts SET Attempts = 0 WHERE IBAN = '{iban}' ")
+            moneybank.commit()
+            return (make_response(jsonify("Card Blocked!")), 403)
+
+        # check attempts
         mycursor.execute(f"SELECT Attempts FROM accounts WHERE IBAN = '{iban}'")
         results = mycursor.fetchone()
 
@@ -37,7 +55,7 @@ def check_balance():
 
             if(int(results[0]) == pincode):
                 mycursor.execute(f"UPDATE accounts SET Attempts = 0 WHERE IBAN = '{iban}' ")
-                
+                moneybank.commit()
                 mycursor.execute(f"SELECT Balance FROM accounts WHERE IBAN = '{iban}'")
                 results= mycursor.fetchone()
                 print(results)
@@ -54,116 +72,89 @@ def check_balance():
                 mycursor.execute(f"SELECT Attempts FROM accounts WHERE IBAN = '{iban}'")
                 results = mycursor.fetchone()
                 print(results)
-                return make_response(jsonify("Wrong pincode " + str(3 - newAttempt)), 401)
+                return make_response(jsonify( int(3 - newAttempt)), 401)
         else:
             mycursor.execute(f"UPDATE accounts SET Attempts = 0 WHERE IBAN = '{iban}' ")
+            moneybank.commit()
             mycursor.execute(f"UPDATE accounts SET BlockStatus = 1 WHERE IBAN = '{iban}'")
             moneybank.commit()
             return (make_response(jsonify("Card Blocked!")),403)
 
     except:
-
+    
+        
         return(make_response(jsonify("Account does not exist")),404)
 
-@app.route('/withdraw', methods = ['GET', 'POST'])
+@app.route('/withdraw', methods=['GET', 'POST'])
 def withdraw():
     try:
-        data= request.json
+        data = request.json
         if not "IBAN" in data or not "Pincode" in data or not "Amount":
-            return(make_response(jsonify("Wrong body!"), 400))
+            return make_response(jsonify("Wrong body!"), 400)
 
         iban = data["IBAN"]
         pincode = data["Pincode"]
         amount = data["Amount"]
+
+        # check block status
+        mycursor.execute(f"SELECT BlockStatus FROM accounts WHERE IBAN = '{iban}'")
+        results = mycursor.fetchone()
+
+        if (int(results[0]) == 1):
+            mycursor.execute(f"UPDATE accounts SET Attempts = 0 WHERE IBAN = '{iban}' ")
+            moneybank.commit()
+            return (make_response(jsonify("Card Blocked!")), 403)
+
+        # check attempts
         mycursor.execute(f"SELECT Attempts FROM accounts WHERE IBAN = '{iban}'")
         results = mycursor.fetchone()
 
         print(results[0])
         if (int(results[0]) < 3):
-            # if (iban == f"{iban}"):
             mycursor.execute(f"SELECT pinCode FROM accounts WHERE IBAN = '{iban}'")
             results = mycursor.fetchone()
 
-            if(int(results[0]) == pincode):
-                if(amount > 300):
-                    return(make_response(jsonify("Amount exceeded!")),400 )
-                
+            if (int(results[0]) == pincode):
+                # correcte pincode
                 mycursor.execute(f"UPDATE accounts SET Attempts = 0 WHERE IBAN = '{iban}' ")
-                
+                moneybank.commit()
                 mycursor.execute(f"SELECT Balance FROM accounts WHERE IBAN = '{iban}'")
-                results= mycursor.fetchone()
-                print(results)
+                results = mycursor.fetchone()
+
+                if (int(results[0]) < amount):
+                    return make_response(jsonify("insufficient balance!"), 406)
+                
                 newbalance = results[0] - amount
                 mycursor.execute(f"UPDATE accounts SET Balance = '{newbalance}'  WHERE IBAN = '{iban}' ")
                 moneybank.commit()
-                return make_response(jsonify(results[0]), 200)
+                # bedrag van balans afhalen
+                return make_response(jsonify(newbalance), 200)
             else:
+                # foutieve pincode
                 mycursor.execute(f"SELECT Attempts FROM accounts WHERE IBAN = '{iban}'")
                 results = mycursor.fetchone()
                 newAttempt = int(results[0]) + 1
 
                 mycursor.execute(f"UPDATE accounts SET Attempts = '{newAttempt}' WHERE IBAN = '{iban}' ")
-
                 moneybank.commit()
 
                 mycursor.execute(f"SELECT Attempts FROM accounts WHERE IBAN = '{iban}'")
                 results = mycursor.fetchone()
                 print(results)
-                return make_response(jsonify("Wrong pincode " + str(3 - newAttempt)), 401)
+                return make_response(jsonify(3 - newAttempt), 401)
         else:
+            # account blokkeren
             mycursor.execute(f"UPDATE accounts SET Attempts = 0 WHERE IBAN = '{iban}' ")
+            moneybank.commit()
             mycursor.execute(f"UPDATE accounts SET BlockStatus = 1 WHERE IBAN = '{iban}'")
             moneybank.commit()
-            return (make_response(jsonify("Card Blocked!")),403)
+            return (make_response(jsonify("Card Blocked!")), 403)
 
     except:
-
-        return(make_response(jsonify("Account does not exist")),404)
-
-@app.route('/pinCheck', methods=['GET', 'POST'])
-def pinCode():
-    try:
-        data = request.json
-        if not "IBAN" in data:
-            return (make_response(jsonify("Foute body!"), 400))
-        iban = data["IBAN"]
-
-        mycursor.execute(f"SELECT pinCode FROM accounts WHERE IBAN = '{iban}'")
-        results = mycursor.fetchone()
-        print(results[0])
-        return (make_response(jsonify(results[0]), 200))
-    except:
+        
         return (make_response(jsonify("Account does not exist")), 404)
 
-@app.route('/checkBlockStatus', methods=['GET', 'POST'])
-def check_blockStatus():
-    try:
-        data = request.json
-        if not "IBAN" in data:
-            return (make_response(jsonify("Foute body!"), 400))
-        iban = data["IBAN"]
 
-        mycursor.execute(f"SELECT BlockStatus FROM accounts WHERE IBAN = '{iban}'")
-        results = mycursor.fetchone()
-        print(results[0])
-        return (make_response(jsonify(results[0]), 200))
-    except:
-        return (make_response(jsonify("Account does not exist")), 404)
-
-@app.route('/blockCard', methods=['GET', 'POST'])
-def check_blockCard():
-    try:
-        data = request.json
-        if not "IBAN" in data:
-            return (make_response(jsonify("Foute body!"), 400))
-        iban = data["IBAN"]
-
-        mycursor.execute(f"UPDATE accounts SET BlockStatus = 1 WHERE IBAN = '{iban}'")
-        results = mycursor.fetchone()
-        print(results[0])
-        return (make_response(jsonify(results[0]), 200))
-    except:
-        return (make_response(jsonify("Account does not exist")), 404)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 8000)
